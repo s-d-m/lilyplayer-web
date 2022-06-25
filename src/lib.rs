0e50b2dbba9044ac68ed99e265c2b75aad19dcc4
Samuel DA MOTA <da.mota.sam@gmail.com>
2022-06-25 20:22:24 +0200

    take file to serve by downloading them instead of including them in the app

--- src/lib.rs
+++ src/lib.rs
@@ -1,45 +1,69 @@
 #[macro_use]
 extern crate rocket;
 
-use rocket::request::Request;
-use rocket::response::{self, Response, Responder};
 use rocket::http::ContentType;
-
+use rocket::request::Request;
+use rocket::response::{self, Responder, Response};
 use shuttle_service::ShuttleRocket;
 
+#[macro_use]
+extern crate lazy_static;
+
+fn download_files(path: &str) -> Option<String> {
+    let response = ureq::get(path).call();
+    let body = match response {
+        Ok(response) => response.into_string(),
+        Err(E) => {
+            println!("Error occurred {}", E);
+            return None;
+        }
+    };
+    match body {
+        Ok(string_value) => Some(string_value),
+        Err(E) => {
+            println!("Error occurred {}", E);
+            None
+        }
+    }
+}
+
+lazy_static! {
+    static ref main_file: Option<String> = {
+        let first_file = download_files("https://");
+        first_file
+    };
+}
 
 #[derive(PartialEq, Eq)]
 enum RequestedFile {
     MainLilyplayer,
-    WorkerJs
+    WorkerJs,
 }
 
 pub struct MyFile(RequestedFile);
 
 impl<'r> Responder<'r, 'static> for MyFile {
     fn respond_to(self, _req: &'r Request<'_>) -> response::Result<'static> {
-        let main_html = include_str!("lilyplayer.html");
-        let worker_js = include_str!("lilyplayer.worker.js");
-
-
-        let (content_type, data) =
-            if self.0 == RequestedFile::MainLilyplayer {
-                (ContentType::HTML, main_html)
-            } else if self.0 == RequestedFile::WorkerJs {
-                (ContentType::JavaScript, worker_js)
-            } else {
-                return Err(rocket::http::Status::NotFound);
-            };
+        let (content_type, data) = if self.0 == RequestedFile::MainLilyplayer {
+            (ContentType::HTML, &*main_file)
+        } else if self.0 == RequestedFile::WorkerJs {
+            (ContentType::HTML, &*main_file)
+        //            (ContentType::JavaScript, Some(String::from(worker_js)))
+        } else {
+            return Err(rocket::http::Status::NotFound);
+        };
 
         let mut response = Response::build();
         response.header(content_type);
-        response.raw_header_adjoin("Cross-Origin-Opener-Policy",  "same-origin");
+        response.raw_header_adjoin("Cross-Origin-Opener-Policy", "same-origin");
         response.raw_header_adjoin("Cross-Origin-Embedder-Policy", "require-corp");
-        response.streamed_body(data.as_ref());
+        match data {
+            Some(x) => response.streamed_body(x.as_bytes()),
+            None => &response,
+        };
         Ok(response.finalize())
     }
 }
-
 
 #[get("/myfile/<path>")]
 fn myfile(path: &str) -> Option<MyFile> {
@@ -52,11 +76,9 @@ fn myfile(path: &str) -> Option<MyFile> {
     }
 }
 
-
 #[shuttle_service::main]
 async fn init() -> ShuttleRocket {
-    let rocket = rocket::build()
-        .mount("/", routes![myfile]);
+    let rocket = rocket::build().mount("/", routes![myfile]);
 
     Ok(rocket)
 }
